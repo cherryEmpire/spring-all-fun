f38c93b4bb57f21858566bd0c71238ebd6beb1ff
Cherry <180835910@qq.com>
2021-02-07 16:40:03 +0800

    add some folder.

---
+++ leetcode-problemset/src/main/java/com/cherry/leetcode/array/Issue1046.java
@@ -0,0 +1,26 @@
+package com.cherry.leetcode.array;
+
+/**
+ * @Author: Cherry
+ * @Date: 2020/12/30
+ * @Desc: Issue1046 最后一块石头的重量
+ * TODO
+ */
+public class Issue1046 {
+    public int lastStoneWeight(int[] stones) {
+        if (stones == null || stones.length == 0) {
+            return 0;
+        }
+        int x;
+        int y;
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 999; i++) {
+            System.out.println((int)((Math.random()*9+1)*100000));
+        }
+
+    }
+}
