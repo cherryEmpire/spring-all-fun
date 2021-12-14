e93f2c634324d0f901d7962cdd377840d20a9850
Cherry <xinhao_weng@163.com>
2021-12-14 09:56:19 +0800

    init neo4j demo

---
+++ neo4j-demo/src/main/java/com/cherry/neo4j/Neo4jDemo.java
@@ -0,0 +1,21 @@
+package com.cherry.neo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+/**
+ * @Author : 翁鑫豪
+ * @Date : 2021/12/13 13:44
+ * @Description: com.cherry.neo4j.Neo4jDemo
+ */
+@EnableOpenApi
+@SpringBootApplication
+@ComponentScan(basePackages = "com.cherry.*")
+public class Neo4jDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Neo4jDemo.class, args);
+    }
+}
